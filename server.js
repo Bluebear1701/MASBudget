23d1184406d7def13fcd496a83c91325caeb465a
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-06-19 21:25:54 -0500

    code

--- server.js
+++ server.js
@@ -16,12 +16,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI ||'mongodb://localhost',
+mongoose.connect(MONGODB_URI,
 {
   useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+  useUnifiedTopology:true,
+  useFindAndModify: false  
 });
 
 // routes
