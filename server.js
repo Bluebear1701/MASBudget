f57a9a9cd6ba6ae43b84ee42fa959f7758d036f8
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-06-19 20:11:46 -0500

    trying something different

--- server.js
+++ server.js
@@ -16,12 +16,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI ||'mongodb://localhost',
+mongoose.connect(MONGODB_URI,
 {
   useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+    useUnifiedTopology: true,  
 });
 
 // routes
