972950d93133fe3e08e6397eab0bcf9a5e145978
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-06-19 21:09:22 -0500

    updated dependices

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,4 +1,4 @@
-/ create global variable for db connection
+// create global variable for db connection
 let db;
 
 // establish a connection to IndexedDB database called 'budget' and set it to version 1
--- server.js
+++ server.js
@@ -16,10 +16,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI,
+mongoose.connect(MONGODB_URI ||'mongodb://localhost',
 {
   useNewUrlParser: true,
-    useUnifiedTopology: true,  
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 });
 
 // routes
